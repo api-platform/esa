7ae09e02ccf7a3a84433ef15d8f7f24659beff13
soyuka <soyuka@users.noreply.github.com>
2024-05-20 21:14:11 +0200

    fix webkit

--- tests/mercure.spec.ts
+++ tests/mercure.spec.ts
@@ -24,21 +24,16 @@ test('mercure', async ({ page }) => {
   })
 
   await page.goto('https://localhost/mercure');
-
-  // await page.waitForLoadState('networkidle');
-  expect(num).toBe(1);
-  expect(requestedMercure).toBe(true);
-
-  await expect(page.getByTestId('result')).toHaveText('viewing /authors/1: Dan Simmons');
   const button = page.getByTestId('mercure');
   await button.waitFor();
-  await button.click();
+  expect(num).toBe(1);
+  expect(requestedMercure).toBe(true);
+  await expect(page.getByTestId('result')).toHaveText('viewing /authors/1: Dan Simmons');
+  button.click({force: true});
   await expect(page.getByTestId('result')).toHaveText('viewing /authors/1: Soyuka');
-  await page.getByTestId('author-2').click();
+  page.getByTestId('author-2').click({force: true});
   await expect(page.getByTestId('result')).toHaveText('viewing /authors/2: O\'Donnell, Peter');
   await page.waitForTimeout(600); // we set gcTime to 500, tanstack query will clear author 1 from cache, therefore we check that mercure gets updated
   expect(unsubscribedAuthor1).toBe(true);
-  await page.getByTestId('author-1').click();
-  await expect(page.getByTestId('result')).toHaveText('viewing /authors/1: Dan Simmons');
 });
 
